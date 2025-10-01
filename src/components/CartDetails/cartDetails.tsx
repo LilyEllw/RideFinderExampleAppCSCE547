80b521d7332549783abcedb01d798501945aa170
Elizabeth Ellwood <christian.ellwood@capgemini.com>
2025-10-01 16:09:06 -0400

    Cart working as intended

--- src/components/CartDetails/cartDetails.tsx
+++ src/components/CartDetails/cartDetails.tsx
@@ -1,18 +1,36 @@
 import CartService from "../../services/cartService";
 import CartCard from "../cartCard/cartCard";
 import { CartItem } from '../../models/cartItem';
+import { useState } from "react";
 
 type CartDetailsProps = {
 	cartService: CartService
 }
 
-export default function cartDetails(props: CartDetailsProps) {
+export default function CartDetails(props: CartDetailsProps) {
 	const { cartService } = props;
+
     //Pulling from local storage as source of truth
-    const cart = cartService.loadCart();
+    const [cart, setCart] = useState(cartService.loadCart());
 	
+    const updateCartItem = (newCartItem: CartItem) => {
+        const item = cart.find((item: CartItem) => item.park.id === newCartItem.park.id);
+        cartService.updateCart(item, newCartItem);
+        setCart(cartService.loadCart());
+    }
+
+    const deleteCartItem = (item: CartItem) => {
+        cartService.removeItemFromCart(item);
+        setCart(cartService.loadCart());
+    }
+
+    const getTotalPrice = () => {
+        
+    }
 	
     return(
-        <div>{cart.map(((item: CartItem) => <CartCard cartItem={item} />))}</div>
+        <div>
+            {cart.map(((item: CartItem) => <CartCard cartItem={item} updateFn={(e) => updateCartItem(e)} deleteFn={deleteCartItem} />))}    
+        </div>
     )
 }
--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -16,8 +16,8 @@ export default class CartService {
     }
 
     addItemToCart = (newItem: CartItem) => {
-        const cart = this.getCart() || [];
-        const itemInCart = cart.findIndex((item) => item.park.id === newItem.park.id);
+        const cart = this.loadCart() || [];
+        const itemInCart = cart.findIndex((item: CartItem) => item.park.id === newItem.park.id);
         if(itemInCart > -1) {
             this.updateCart(cart[itemInCart], newItem);
         }
@@ -26,13 +26,14 @@ export default class CartService {
     }
 
     removeItemFromCart = (remItem: CartItem) => {
-        const cart = this.getCart();
-        cart.filter((val) => !(val.park.id === remItem.park.id || val.numDays === remItem.numDays))
-        this.save(cart);
+        const cart = this.loadCart();
+        
+        const result = cart.filter((val: CartItem) => val.park.id !== remItem.park.id)
+        this.save(result);
     }
 
     updateCart(oldItem: CartItem, newItem: CartItem) {
-        const cart = this.getCart()
+        const cart = this.loadCart();
         if(oldItem.park.id !== newItem.park.id) {
             this.save(cart);
         }
@@ -44,7 +45,7 @@ export default class CartService {
             numAdults: newItem.numAdults || oldItem.numAdults,
             numKids: newItem.numKids || oldItem.numKids
         };
-        const index = cart.findIndex((val) => val.park.id === combinedItem.park.id);
+        const index = cart.findIndex((val: CartItem) => val.park.id === combinedItem.park.id);
         if(index > -1) {
             cart[index] = combinedItem;
         }
